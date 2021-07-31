a4d398648f2a71f744b734fb197d319fa157dbb6
beverlily <beverly.li@live.com>
2021-07-31 15:03:43 -0400

    (refactor) clean up + formatting

--- script.js
+++ script.js
@@ -1,287 +1,288 @@
-var score = 0;
-var maxDrops = 50;
-var drops = maxDrops;
-var gameWidth;
-var basketWidth;
-var maxSnow = 25;
+let score;
+let drops;
+let gameWidth;
+let basketWidth;
 
-//Variables for timers
-var fallingSnow;
-var fallingDrop;
-var checkCollision;
+const maxDrops = 100;
+const maxSnow = 30;
 
-$(document).ready(function() {
-    //Start Game
-    $("#game-start").hide();
-    $("#game-over").hide();
-    $("#start-btn").click(startGame);
-    $("#start-btn").keyup(function(e){
-        if (e.keyCode == 13) {
-            startGame();
-        }
+// Variables for timers
+let fallingSnow;
+let fallingDrop;
+let checkCollision;
+
+$(document).ready(function () {
+  showInitialScreen();
+  setStartButton();
+});
+
+const showInitialScreen = () => {
+  $("#game-start").hide();
+  $("#game-over").hide();
+  $("#instructions-page").show();
+};
+
+const setStartButton = () => {
+  $("#start-btn").click(startGame);
+  $("#start-btn").keyup(function (e) {
+    if (e.keyCode == 13) startGame();
+  });
+};
+
+const startGame = () => {
+  resetGame();
+  setScore();
+  fadeInMusic();
+
+  moveBasketListener();
+
+  clearInterval(checkCollision);
+  checkCollision = setInterval(collision, 0);
+  collision();
+
+  startSnow();
+  startDrop();
+  setBackButton();
+};
+
+const moveBasketListener = () => {
+  $(document).mousemove((e) => {
+    gameWidth = $("#game").width();
+    basketWidth = $("#basket").width();
+
+    // x-coord of mouse relative to doc - x-coord of game container relative to doc
+    // This is because basket is position relative to game container
+    let leftValue = e.pageX - $("#game").offset().left;
+
+    // To make sure that basket doesn't go out of the game container
+    const minLeft = 0;
+    const maxLeft = gameWidth - basketWidth;
+
+    if (leftValue < minLeft) {
+      leftValue = minLeft;
+    } else if (leftValue > maxLeft) {
+      leftValue = maxLeft;
+    }
+
+    $("#basket").css({ left: leftValue });
+  });
+};
+
+const collision = () => {
+  $(".drop").each(function () {
+    const basketTop = $("#basket").position().top;
+    const dropTop = $(this).position().top;
+    const dropPos = $(this).position().left;
+    const basketLeftCorner = $("#basket").position().left;
+    const basketRightCorner = $("#basket").position().left + basketWidth;
+
+    const dropInBasket =
+      dropTop >= basketTop &&
+      dropPos >= basketLeftCorner &&
+      dropPos <= basketRightCorner;
+
+    if (dropInBasket) {
+      playDropSound();
+      removeDrop(this);
+      updateScore();
+    }
+  });
+};
+
+const playDropSound = () => {
+  const dropSound = $("#drop-sound")[0];
+  dropSound.currentTime = 0;
+  dropSound.volume = 0.25;
+  dropSound.play();
+};
+
+const removeDrop = (drop) => {
+  $(drop).remove();
+  $("#basket").addClass("water");
+};
+
+const updateScore = () => {
+  score++;
+  setScore();
+};
+
+const resetGame = () => {
+  $("#game-over").hide();
+  $("#instructions-page").hide();
+  $("#basket").removeClass("water");
+  $("#game-start").show();
+
+  score = 0;
+  drops = 0;
+};
+
+const fadeInMusic = () => {
+  $("#background-music")[0].volume = 0.1;
+  $("#background-music")[0].play();
+  $("#background-music").animate({ volume: 0.5 }, 0.0);
+};
+
+const setScore = () => $("#score").html(`SCORE: ${score}`);
+
+const randomizeNumber = (min, max) => Math.random() * (max - min) + min;
+
+const makeSnow = () => {
+  const minSize = 1;
+  const maxSize = 5;
+  const minSpeed = 10;
+  const maxSpeed = 15;
+  const minTop = -200;
+  const maxTop = 100;
+  const minLeft = maxSize;
+  const maxLeft = 500 - maxSize;
+  const minFade = 5;
+  const maxFade = 10;
+  const minSway = 5;
+  const maxSway = 10;
+
+  //Possible glow animations
+  const glowOptions = [
+    "",
+    ", glow 1.5s infinite linear",
+    ", glow 1s infinite linear",
+  ];
+
+  // Possible opacity values
+  const opacityValues = ["0.25", "0.5", "0.75"];
+
+  // Randomized variables within a certain range
+  const top = randomizeNumber(minTop, maxTop);
+  const left = randomizeNumber(minLeft, maxLeft);
+  const size = randomizeNumber(minSize, maxSize);
+  const fade = randomizeNumber(minFade, maxFade);
+  const speed = randomizeNumber(minSpeed, maxSpeed);
+  const sway = randomizeNumber(minSway, maxSway);
+
+  const glow = glowOptions[Math.floor(Math.random() * glowOptions.length)];
+  const opacity =
+    opacityValues[Math.floor(Math.random() * opacityValues.length)];
+
+  const snow = $(document.createElement("div"));
+  snow.addClass("snow");
+  snow.css({
+    height: `${size}px`,
+    width: `${size}px`,
+    top: `${top}px`,
+    left: `${left}px`,
+    border: "1px solid white",
+    opacity: opacity,
+    animation: `fadeIn ${fade}s 1 ease-in forwards, fallFade ${speed}s 1 linear forwards ${glow}, sway ${sway}s infinite`,
+  });
+
+  $("#snow-container").append(snow);
+  // Deletes snow when it's done falling
+  snow.on("animationend", function (e) {
+    const animationName = e.originalEvent.animationName;
+    if (animationName == "fallFade") $(this).remove();
+  });
+};
+
+// Snow
+const startSnow = () => {
+  clearInterval(fallingSnow);
+  $("#snow-container .snow").remove();
+  fallingSnow = setInterval(makeSnow, 1500);
+};
+
+const stopSnow = () => {
+  clearInterval(fallingSnow);
+  // Clear existing snow
+  $("#snow-container .snow").remove();
+};
+
+const makeDrop = () => {
+  if (drops < maxDrops) {
+    const drop = $(document.createElement("img"));
+    drop.attr("src", "images/drop.png");
+    drop.attr("alt", "blue teardrop");
+
+    const dropWidth = 15;
+    const minLeft = dropWidth;
+    const maxLeft = gameWidth - dropWidth;
+    const minSpeed = 0.5;
+    const maxSpeed = 1;
+    const top = dropWidth;
+    const left = randomizeNumber(minLeft, maxLeft);
+    const speed = randomizeNumber(minSpeed, maxSpeed);
+
+    drop.addClass("drop");
+    drop.css({
+      position: "absolute",
+      width: dropWidth,
+      top: `${top}px`,
+      left: `${left}px`,
+      animation: `glow 1.5s infinite linear, fallDrop ${speed}s 1 linear forwards`,
     });
 
-    function startGame() {
-        $("#game-over").hide();
-        $("#instructions-page").hide();
-        $("#basket").removeClass("water");
-        drops = maxDrops;
-        score = 0;
-        $("#score").html("SCORE: " + score);
+    $("#drop-container").append(drop);
+    drops++;
 
-        //https://stackoverflow.com/questions/21611448/how-to-have-fade-in-out-on-audio-html5
-        //Fades in music
-        $('#background-music')[0].volume = 0.1;
-        $('#background-music')[0].play();
-        $('#background-music').animate({
-            volume: 0.5
-        }, 0.0);
+    // Deletes drop when it's done falling
+    drop.on("animationend", function (e) {
+      const animationName = e.originalEvent.animationName;
+      if (animationName === "fallDrop") $(this).remove();
+    });
+  }
+  // If no more drops left
+  else {
+    stopDrop();
+    setTimeout(() => showGameOverModal(), 1000);
+  }
+};
 
-        //Hide instruction page
-        $("#instructions-page").hide();
-        $("#game-start").show();
+const showGameOverModal = () => {
+  $("#game-over").show();
+  $("#game-over-content").hide();
+  $("#game-over-content").html("YOUR SCORE: " + score);
+  $("#game-over-content").show();
+  
+  $(document).keyup(function (e) {
+    if (e.keyCode == 27) $("#game-over").fadeOut();
+  });
 
-        //Moves basket depending on position of mouse
-        $(document).mousemove(function(e) {
-            gameWidth = $("#game").width();
-            basketWidth = $("#basket").width();
+  $("#play-again .button").click(() => playAgain());
 
-            //x-coord of mouse relative to doc - x-coord of game container relative to doc
-            //This is because basket is position relative to game container
-            var leftValue = (e.pageX - $("#game").offset().left);
+  $("#play-again .button").keyup(function (e) {
+    if (e.keyCode == 13) {
+      startGame();
+    }
+  });
+};
 
-            //To make sure that basket doesn't go out of the game container
-            var minLeft = 0;
-            var maxLeft = gameWidth - basketWidth;
+const playAgain = () => {
+  stopSnow();
+  clearExistingDrops();
+  startGame();
+};
 
-            if (leftValue < minLeft) {
-                leftValue = minLeft;
-            } else if (leftValue > maxLeft) {
-                leftValue = maxLeft;
-            }
+// Drop
+const startDrop = () => {
+  stopDrop();
+  clearExistingDrops();
+  fallingDrop = setInterval(makeDrop, 500);
+};
 
-            $('#basket').css({
-                'left': leftValue
-            });
-        });
+const stopDrop = () => clearInterval(fallingDrop);
 
-        clearInterval(checkCollision);
-        checkCollision = setInterval(collision, 0);
+const clearExistingDrops = () => {
+  $("#drop-container .drop").remove();
+};
 
-        function collision() {
-            $(".drop").each(function() {
-                var basketTop = $("#basket").position().top;
-                var dropTop = $(this).position().top;
-                var dropPos = $(this).position().left;
-                var basketLeftCorner = $("#basket").position().left;
-                var basketRightCorner = $("#basket").position().left + basketWidth;
+const setBackButton = () => {
+  $("#back-btn .button").click(() => back());
+  $("#back-btn .button").keyup((e) => {
+    if (e.keyCode == 13) back();
+  });
+};
 
-                //Drop is within the basket container
-                if (dropTop >= basketTop && dropPos >= basketLeftCorner &&
-                    dropPos <= basketRightCorner) {
-
-                    //Referenced https://stackoverflow.com/questions/12953928/immediate-play-sound-on-button-click-in-html-page
-                    //Drop sound every time a drop is catched
-                    var dropSound = $('#drop-sound')[0];
-                    dropSound.currentTime = 0;
-                    dropSound.volume = 0.25;
-                    dropSound.play();
-
-                    //Remove drop
-                    $(this).remove();
-                    $("#basket").addClass("water");
-
-                    //Update score
-                    score += 1;
-                    $("#score").html("SCORE: " + score);
-
-                }
-            });
-        }
-
-        function randomizeNumber(min, max) {
-            return (Math.random() * (max - min)) + min;
-        }
-
-        //Snow
-        function startSnow() {
-            clearInterval(fallingSnow);
-            $("#snow-container .snow").remove();
-            fallingSnow = setInterval(makeSnow, 1500);
-        }
-
-        function stopSnow() {
-            clearInterval(fallingSnow);
-            //Clear existing snow
-            $("#snow-container .snow").remove();
-        }
-
-        //Drop
-        function startDrop() {
-            clearInterval(fallingDrop);
-            //Clear existing drop
-            $("#drop-container .drop").remove();
-            fallingDrop = setInterval(makeDrop, 500);
-        }
-
-        function stopDrop() {
-            clearInterval(fallingDrop);
-        }
-
-        //Starts making snow + drops
-        startSnow();
-        startDrop();
-
-        $("#back-btn .button").click(function() {
-          back();
-        });
-
-        $("#back-btn .button").keyup(function(e){
-          if (e.keyCode == 13){
-            back();
-          }
-        });
-
-        function back(){
-          //Reset values
-          stopDrop();
-          stopSnow();
-          //Clear existing drops
-          $("#drop-container .drop").remove();
-          $("#game-start").hide();
-          $("#game-over").hide();
-          $("#instructions-page").show();
-        }
-
-        function makeSnow() {
-            var minSize = 1;
-            var maxSize = 5;
-            var minSpeed = 10;
-            var maxSpeed = 15;
-            var minTop = -200;
-            var maxTop = 100;
-            var minLeft = maxSize;
-            var maxLeft = 500 - maxSize;
-            var minFade = 5;
-            var maxFade = 10;
-            var minSway = 5;
-            var maxSway = 10;
-
-            //Possible glow animations
-            var glowOptions = [", glow 1.5s infinite linear", "", ", glow 1s infinite linear", ];
-
-            //Possible opacity values
-            var opacityValues = ["0.25", "0.5", "0.75"];
-
-            //Randomized variables within a certain range
-            var top = randomizeNumber(minTop, maxTop);
-            var left = randomizeNumber(minLeft, maxLeft);
-            var size = randomizeNumber(minSize, maxSize);
-            var fade = randomizeNumber(minFade, maxFade);
-            var speed = randomizeNumber(minSpeed, maxSpeed);
-            var sway = randomizeNumber(minSway, maxSway);
-
-            /*Referenced https://stackoverflow.com/questions/5915096/get-random-item-from-javascript-array*/
-            var glow = glowOptions[Math.floor(Math.random() * glowOptions.length)];
-            var opacity = opacityValues[Math.floor(Math.random() * opacityValues.length)];
-
-            //Referenced https://stackoverflow.com/questions/268490/jquery-document-createelement-equivalent
-            var snow = $(document.createElement('div'));
-            snow.addClass("snow");
-            snow.css({
-                "height": size + "px",
-                "width": size + "px",
-                "top": top + "px",
-                "left": left + "px",
-                "border": "1px solid white",
-                "opacity": opacity,
-                "animation": "fadeIn " + fade + "s 1 ease-in forwards, fallFade " +
-                    speed + "s 1 linear forwards " + glow + ", sway " +
-                    sway + "s infinite"
-            });
-
-            $("#snow-container").append(snow);
-
-            //Referenced https://stackoverflow.com/questions/12498686/detect-which-css-animation-just-ended-in-javascript
-            //Deletes snow when it's done falling
-            snow.on("animationend", function(e) {
-                var animationName = e.originalEvent.animationName;
-                if (animationName == "fallFade") {
-                    $(this).remove();
-                }
-            });
-        } //end of makeSnow
-
-        function makeDrop() {
-            if (drops > 0) {
-                var drop = $(document.createElement('img'));
-                drop.attr('src', 'images/drop.png');
-                drop.attr('alt', 'blue teardrop');
-
-                var dropWidth = 15;
-                var minLeft = dropWidth;
-                var maxLeft = gameWidth - dropWidth;
-                var minSpeed = 0.5;
-                var maxSpeed = 1;
-                var top = dropWidth;
-                var left = randomizeNumber(minLeft, maxLeft);
-                var speed = randomizeNumber(minSpeed, maxSpeed);
-
-                drop.addClass("drop");
-                drop.css({
-                    "position": "absolute",
-                    "width": dropWidth,
-                    "top": top + "px",
-                    "left": left + "px",
-                    "animation": "glow 1.5s infinite linear, fallDrop " +
-                        speed + "s 1 linear forwards "
-                });
-
-                $("#drop-container").append(drop);
-                drops -= 1;
-
-                //Deletes drop when it's done falling
-                drop.on("animationend", function(e) {
-                    var animationName = e.originalEvent.animationName;
-                    if (animationName === "fallDrop") {
-                        $(this).remove();
-                    }
-                });
-            }
-            //If no more drops left
-            else {
-                stopDrop();
-                setTimeout(function() {
-                    $("#game-over").show();
-                    $("#game-over-content").hide();
-                    $("#game-over-content").html("YOUR SCORE: " + score);
-                    $("#game-over-content").show();
-                    $(document).keyup(function(e){
-                        if (e.keyCode == 27) {
-                            $("#game-over").fadeOut();
-                        }
-                    });
-
-                    $("#play-again .button").click(function(){
-                      playAgain();
-                    });
-
-                    $("#play-again .button").keyup(function(e){
-                        if (e.keyCode == 13) {
-                            startGame();
-                        }
-                    });
-
-                    function playAgain(){
-                      stopSnow();
-                      //Clear existing drops
-                      $("#drop-container .drop").remove();
-                      $("#game-start").hide();
-                      $("#game-over").hide();
-                      startGame();
-                    }
-                  }, 1000);
-            }
-        } //end of makeDrop
-    } //end of start game
-}); //end of document ready
+const back = () => {
+  stopDrop();
+  stopSnow();
+  clearExistingDrops();
+  showInitialScreen();
+};
